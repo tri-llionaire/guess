028cd6535e61483af0bd256774439ecf682a04fc
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2020-10-02 14:14:40 -0400

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,5 +1,5 @@
 import random, time
-find = random.randint(0, 999999)
+find = random.randint(0, 999999999)
 x = 0
 s = int(input('how many seconds to run: '))
 start = time.time()
@@ -8,23 +8,17 @@ while (time.time() - start) < s:
     x += 1
     #print('guessed {}'.format(guess))
     if guess == find:
-        print('match')
+        print('match: {}'.format(guess))
         break
+end = time.time()
 second = str(x)
-minute = str(x*60)
-hour = str(x*3600)
-day = str(x*86400)
-week = str(x*604800)
-month = str(x*18446400)
-year = str(x*220752000)
-def quick(inputted):
-    inputted = inputted[::-1]
-    i = 0
-    new = ''
-    for y in inputted:
-        if (i % 3) == 0:
-            new = new + ','
-        i += 1
-        new = new + y
-    return new[::-1][:-1]
-print('tried {} times in {}s\nwhich comes out to {} a minute, {} an hour,\n{} a day, {} a week,\n{} a month, and {} a year'.format(quick(second), s, quick(minute), quick(hour), quick(day), quick(week), quick(month), quick(year)))
+use = second[::-1]
+i = 0
+new = ''
+for y in use:
+    if (i % 3) == 0:
+        new = new + ','
+    i += 1
+    new = new + y
+second = new[::-1][:-1]
+print('tried {} times in {}s'.format(second, str(end - start)[:5]))
